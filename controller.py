d38a2ba4b1abe2e7cbe645a095944bf879c7aea8
antoinepetit <apetit@clipper.ens.fr>
2014-01-27 19:22:00 +0100

    Modulation en fréquence 1

--- controller.py
+++ controller.py
@@ -1,17 +1,17 @@
 import os
 
-ploter = "python2 ./Rayleigh_Benard.py"
+ploter = "python  ./Rayleigh_Benard.py"
 niter = 2000
 options = " --nx 200 --ny 100 --behind --rect 15 3 -R --alpha 100 -s 5 --niter "+str(niter)+" -r 50"
-var_parameter = lambda iter: " -S " +  str(10+iter) + " 0.05"
+var_parameter = lambda iter: " -S " +  str(10+iter) + " 0.02"
 out = lambda iter: " --out sequence_" + str(iter)
 verbose = " --drag_only"
 
 simul = lambda i : ploter + options + out(i) + var_parameter(i) + verbose
-pipe = " | tail -n +5 | head -n -1"
+pipe = ""
 stdout = lambda iter : " > sequence_" + str(iter) + "_output"
 
-for i in xrange(10,30,3):
+for i in xrange(10):
     command = simul(i) + pipe + stdout(i)
     print "Launching command " + str(i)
     print command
